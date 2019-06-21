231283ca8a245775bc4a0504f94d55202a9e469b
xingtingbiao <18551617816@163.com>
2019-06-21 17:33:00 +0800

    feature: DataSet train

--- flink-train/src/test/scala/com/xtb/flink/test/TestScala.scala
+++ flink-train/src/test/scala/com/xtb/flink/test/TestScala.scala
@@ -1,5 +1,6 @@
 package com.xtb.flink.test
 
+import org.apache.flink.api.scala._
 import org.junit.Test
 
 @Test
@@ -8,5 +9,14 @@ class TestScala {
   @Test
   def hello(): Unit = {
     println("hello world")
+  }
+
+  @Test
+  def test(): Unit = {
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    val value = env.fromCollection(List(("abc", List("1", "2")), ("def", List("3", "4"))))
+    value.flatMap(x => {
+      x._2.map((x._1, _))
+    }).print()
   }
 }
