2bdf23df124c390a9267f8f39a6814bcfaf56329
xingtingbiao <18551617816@163.com>
2019-06-24 17:02:34 +0800

    feature: DataStream

---
+++ flink-train/src/main/scala/com/xtb/flink/course05/DataStreamWCScalaApp.scala
@@ -0,0 +1,56 @@
+package com.xtb.flink.course05
+
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
+
+object DataStreamWCScalaApp {
+
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+//    socketStreamFunction(env)
+//    nonParallelSourceFunction(env)
+//    parallelSourceFunction(env)
+//    richParallelSourceFunction(env)
+    splitSelectFunction(env)
+
+    env.execute("DataStreamWCScalaApp")
+  }
+
+  // 这里的addSource不能设置并行度
+  def nonParallelSourceFunction(env: StreamExecutionEnvironment) = {
+    val value = env.addSource(new MyNonParallelSourceFunction).setParallelism(2)
+    value.print()
+  }
+
+  def parallelSourceFunction(env: StreamExecutionEnvironment) = {
+    val value = env.addSource(new MyParallelSourceFunction).setParallelism(2)
+    value.print()
+  }
+
+  def richParallelSourceFunction(env: StreamExecutionEnvironment) = {
+    val value = env.addSource(new MyRichParallelSourceFunction).setParallelism(2)
+    value.print()
+  }
+
+  def socketStreamFunction(env: StreamExecutionEnvironment) = {
+    val text = env.socketTextStream("localhost", 9999)
+    text.flatMap(_.toLowerCase.split("\\W+").filter(_.nonEmpty))
+      .map((_, 1))
+      .keyBy(0)
+      .timeWindow(Time.seconds(5))
+      .sum(1)
+      .print()
+  }
+
+  def splitSelectFunction(env: StreamExecutionEnvironment): Unit = {
+    val value = env.addSource(new MyNonParallelSourceFunction)
+    val splits = value.split(
+      num =>
+        num % 2 match {
+          case 0 => List("even")
+          case 1 => List("old")
+        }
+    )
+    splits.select("even").print()
+  }
+}
---
+++ flink-train/src/main/scala/com/xtb/flink/course05/MyRichParallelSourceFunction.scala
@@ -0,0 +1,20 @@
+package com.xtb.flink.course05
+
+import org.apache.flink.streaming.api.functions.source.{RichParallelSourceFunction, SourceFunction}
+
+class MyRichParallelSourceFunction extends RichParallelSourceFunction[Long] {
+  var isRunning = true
+  var count = 1L
+
+  override def run(ctx: SourceFunction.SourceContext[Long]): Unit = {
+    while (isRunning & count < 1000) {
+      ctx.collect(count)
+      count += 1
+      Thread.sleep(1000)
+    }
+  }
+
+  override def cancel(): Unit = {
+    isRunning = false
+  }
+}
